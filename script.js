54128447f2ea07f151a5dde89e23397010059af8
rishi2025 <99127485+rishi2025@users.noreply.github.com>
2023-08-16 13:53:54 +0530

    Added all basic functionalities

--- script.js
+++ script.js
@@ -67,10 +67,12 @@ const currDate = document.querySelector('.currDate');
 const amt = document.querySelector('.amt');
 const transferTo = document.querySelector('.inputUsername');
 const transferAmt = document.querySelector('.inputAmount');
+const transferGo = document.querySelector('.transferGo');
 const inputLoan = document.querySelector('.inputLoan');
 const requestGo = document.querySelector('.requestGo');
 const deleteUsername = document.querySelector('.deleteUsername');
 const deletePassword = document.querySelector('.deletePassword');
+const deleteGo = document.querySelector('.deleteGo');
 const income = document.querySelector('.incomeAmt');
 const outgo = document.querySelector('.outgoAmt');
 const interestV = document.querySelector('.interestAmt');
@@ -86,12 +88,24 @@ const createUserNames = function(accs) {
     });
 };
 
+createUserNames(accounts);
+
+const createGlobalAmt = function(accs) {
+    accs.forEach(function (accountActive) {
+        accountActive.globalAmt = accountActive.movements
+            .reduce((acc, curr) => acc + curr, 0);
+    });
+};
+
+createGlobalAmt(accounts);
+
 const init = function () {
     document.querySelector('.app').style.opacity = 0;
     loginPassword.value = '';
     loginUsername.value = '';
     loginPassword.blur();
     loginUsername.blur();
+    headerName.textContent = "Nested Bank";
 }
 
 const displayError = function (s) {
@@ -112,8 +126,6 @@ document.querySelector('.overlay').addEventListener('click', function () {
     document.querySelector('.popup').classList.remove('popupFlex');
     document.querySelector('.popup').classList.add('hidden');
 });
-
-createUserNames(accounts);
 
 const update = function (account) {
     document.querySelector('.app').style.opacity = 100;
@@ -139,7 +151,6 @@ const update = function (account) {
 
     // returns sum of movements
     const balance = movements.reduce((acc, curr) => acc + curr, 0);
-    amt.textContent = `${balance} $`;
 
     const calcStats = function() {
         const incomeAmt = movements
@@ -158,7 +169,9 @@ const update = function (account) {
             .filter(int => int >= 1)
             .reduce((acc, mov) => acc + mov, 0);
         const int = Math.trunc(interestAmt);
-        interestV.textContent = `${ int + (Math.trunc((interestAmt - int) * 100)) / 100} $`;
+        interestV.textContent = `${int + (Math.trunc((interestAmt - int) * 100)) / 100} $`;
+        account.globalAmt = balance + int + (Math.trunc((interestAmt - int) * 100)) / 100;
+        amt.textContent = `${account.globalAmt} $`;
     };
     calcStats();
 };
@@ -171,9 +184,6 @@ login.addEventListener('click', function (e) {
 
     currAccount = accounts.find(acc => (acc.username === loginUsername.value
         && acc.password === loginPassword.value));
-
-    console.log(currAccount);
-    console.log(loginPassword);
 
     if (currAccount)
         update(currAccount);
@@ -188,4 +198,120 @@ login.addEventListener('click', function (e) {
     loginUsername.value = '';
     loginPassword.blur();
     loginUsername.blur();
+});
+
+let transferAccount;
+transferGo.addEventListener('click', function (e) {
+    //Prevents form from submitting
+    e.preventDefault();
+
+    transferAccount = accounts.find(acc => (acc.username === transferTo.value));
+
+    if (!currAccount)
+        displayError("YOU ARE NOT LOGGED IN");
+
+    else {
+
+        if (transferAccount) {
+            if (transferAccount === currAccount)
+                displayError("YOU CANT TRANSFER TO YOURSELF");
+            else {
+                if (transferAmt.value) {
+                    if (transferAmt.value > currAccount.globalAmt)
+                        displayError("YOU DONT HAVE ENOUGH BALANCE");
+                    else if ((transferAmt.value < 0))
+                        displayError("NEGATIVE TRANSFER AMOUNT IS INVALID");
+                    else if ((transferAmt.value == 0))
+                        displayError("ZERO TRANSFER AMOUNT IS INVALID");
+                    else
+                    {
+                        currAccount.movements.push(-Number(transferAmt.value));
+                        transferAccount.movements.push(Number(transferAmt.value));
+                        update(currAccount);
+                    }
+                }
+            }
+        }
+
+        else
+            displayError("USER DOES NOT EXIST");
+
+        transferAmt.value = '';
+        transferTo.value = '';
+        transferAmt.blur();
+        transferTo.blur();
+    }
+});
+
+const valid = function (deposits) {
+    let mx = 0;
+    deposits.forEach(function (i) {
+        mx = Math.max(mx, i);
+    });
+    return mx;
+};
+
+requestGo.addEventListener('click', function (e) {
+    //Prevents form from submitting
+    e.preventDefault();
+
+    const loan = inputLoan.value;
+    if (!currAccount)
+        displayError("YOU ARE NOT LOGGED IN");
+
+    else {
+        if (loan) {
+            if (loan <= 0)
+                displayError("INVALID LOAN REQUEST");
+            else if (valid(currAccount.movements) * 3 === 0)
+                displayError("WE CAN NOT GIVE YOU LOAN");
+            else if (loan > (valid(currAccount.movements) * 3))
+                displayError(`YOUR ACCOUNT CAN GET A LOAN OF ${valid(currAccount.movements) * 3} $ AT MAX.`);
+            else
+            {
+                currAccount.movements.push(Number(loan));
+                update(currAccount);
+            }
+        }
+        else
+            displayError("INVALID LOAN REQUEST");
+    }
+    inputLoan.value = '';
+    inputLoan.blur();   
+});
+
+deleteGo.addEventListener('click', function (e) {
+    //Prevents form from submitting
+    e.preventDefault();
+
+    const deleteAccount = deleteUsername.value;
+    const deletePin = deletePassword.value;
+
+    if (!currAccount)
+        displayError("YOU ARE NOT LOGGED IN");
+
+    else {
+        if (deleteAccount) {
+            if (deleteAccount === currAccount.username)
+            {
+                if (deletePin === currAccount.password) {
+                    const index = accounts.findIndex(acc => acc.username === currAccount.username);
+                    accounts.splice(index, 1);
+                    displayError("ACCOUNT CLOSED SUCCESSFULLY");
+                    console.log(accounts);
+                    init();
+                }
+                else
+                    displayError("INCORRECT PASSWORD");
+            }
+            else
+                displayError("YOU CAN ONLY REQUEST TO CLOSE YOUR ACCOUNT")
+        }
+        else
+            displayError("INVALID USERNAME");
+    }
+    deleteUsername.value = '';
+    deleteUsername.blur();   
+    deletePassword.value = '';
+    deletePassword.blur();   
 });
